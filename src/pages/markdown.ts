a387672925f8d054dc91fc18165f7d4a3d823810
Simon He <674949287@qq.com>
2025-11-21 11:22:06 +0800

    chore: lint

--- src/pages/markdown.ts
+++ src/pages/markdown.ts
@@ -1,6 +1,7 @@
+// historical alias used in tests
+import { typescriptContent } from '../samples/content-typescript.js'
+
 export { markdownContent } from '../samples/content-markdown.js'
 export { phpContent } from '../samples/content-php.js'
 export { typescriptContent, vueContent } from '../samples/content-typescript.js'
-// historical alias used in tests
-import { typescriptContent } from '../samples/content-typescript.js'
 export const streamContent = typescriptContent
